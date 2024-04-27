6ed907005a1e186f625b643b07ea96054f509264
Nicat Ceferov <nicat.ceferov14@gmail.com>
2024-04-27 13:07:41 +0400

    Yeni API ve bezi elaveler

--- main.js
+++ main.js
@@ -1,13 +1,31 @@
-async function logNews() {
-    const response = await fetch("https://newsapi.org/v2/everything?q=tesla&from=2024-03-23&sortBy=publishedAt&apiKey=e5208be4d0034433a6e45ef5e2de11b6");
-    const news = await response.json();
+apikey = '83fe06d30fe3cc51baf2c4c735cf921d';
+url = 'https://gnews.io/api/v4/search?q=example&lang=en&country=us&max=10&apikey=' + apikey;
 
-    xeberler = document.getElementsByClassName('xeberler');
+fetch(url)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        articles = data.articles;
 
-    for (let i = 0; i < 10; i++) {
-        if (news.articles[i].urlToImage != null) {
-            xeberler[i].src = news.articles[i].urlToImage;
+        xeberler = document.getElementsByClassName('xeberler');
+
+        for (i = 0; i < 10; i++) {
+            // articles[i].title
+            console.log("Title: " + articles[i]['title']);
+            // articles[i].description
+            console.log("Description: " + articles[i]['description']);
+            xeberler[i].src = articles[i]['image'];
+
         }
-    }
-  }
-logNews();
+
+        latestnews = document.getElementById('latestnews');
+        latesttitle = document.getElementsByClassName('latest-title');
+        latestdesc = document.getElementsByClassName('latest-desc');
+
+        for (let i = 0; i < latestnews.children.length; i++) {
+            latestnews.children[i].firstElementChild.src = articles[i + 5]['image'];
+            latesttitle[i].textContent = articles[i + 5]['title'];
+            latestdesc[i].textContent = articles[i + 5]['description'];
+        }
+    });
